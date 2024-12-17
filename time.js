a6f0fcadc02728bfdee8147648e62006280eebf0
Sita Cicirko <ns.cicirko@gmail.com>
2024-12-17 13:22:11 +0000

    second commit

--- time.js
+++ time.js
@@ -1,11 +1,22 @@
 const button = document.getElementById("button");
 const message = document.getElementById("message");
 
-button.textContent = "Click Me";
+button.textContent = "Click me to start the timer";
+let seconds = 0;
 
 button.addEventListener("click", function () {
   message.textContent = "Yay! Check the console!";
   console.log("Its alive!");
+
+  const oneSecondInterval = setInterval(function () {
+    seconds++;
+    message.textContent = `Time: ${seconds} seconds`;
+  }, 1000);
+
+  setTimeout(function () {
+    clearInterval(oneSecondInterval);
+    console.log("The interval has been stopped after 5 seconds.");
+  }, 5000);
 
   setTimeout(function () {
     console.log("Not anymore");
